25f1b4321cb8bf7f49106cd2b57e040f52d8e0f4
Frostetsky <nesterenko-nikita@bk.ru>
2020-10-20 03:25:44 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#342932]

--- src/main/java/grabber/SqlRuParse.java
+++ src/main/java/grabber/SqlRuParse.java
@@ -6,18 +6,28 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SqlRuParse {
+
+    private static String url = "https://www.sql.ru/forum/job-offers";
+    private static final int pages = 5;
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements date = doc.select(".altCol");
-        int index = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(date.get(index).text());
-            index +=2;
-            System.out.println(System.lineSeparator());
+        for (int i = 1; i <= pages; i++) {
+            Document doc = Jsoup.connect(SqlRuParse.pages(url, i)).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements date = doc.select(".altCol");
+            int index = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(date.get(index).text());
+                index += 2;
+                System.out.println(System.lineSeparator());
+            }
         }
+    }
+
+    private static String pages(String url, int i) {
+        return url + "/" + i;
     }
 }
